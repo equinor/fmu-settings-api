b2998ffbb6f48a1e627c21d68962163f7de41441
mferrera <matt.ferrera@gmail.com>
2025-06-18 07:14:51 +0200

    ENH: Add smda/masterdata route to fetch masterdata

--- src/fmu_settings_api/v1/main.py
+++ src/fmu_settings_api/v1/main.py
@@ -6,7 +6,8 @@ from fmu_settings_api.deps import get_session, get_smda_session
 from fmu_settings_api.models import Ok
 from fmu_settings_api.v1.responses import GetSessionResponses
 
-from .routes import project, session, smda, user
+from .routes import project, session, user
+from .routes.smda import main as smda
 
 api_v1_router = APIRouter()
 api_v1_router.include_router(project.router, dependencies=[Depends(get_session)])
