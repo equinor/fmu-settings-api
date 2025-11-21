66b26e4aa566b77baeea88a7aad7fd7dcad03364
Muhammad Gibran Alfarizi <muga@equinor.com>
2025-11-21 11:45:59 +0100

    TST: Fix circular import

--- src/fmu_settings_api/services/__init__.py
+++ src/fmu_settings_api/services/__init__.py
@@ -1,7 +1,6 @@
 """Services the end points need to use."""
 
 from fmu_settings_api.services.project import ProjectService
-from fmu_settings_api.services.session import SessionService
 from fmu_settings_api.services.smda import SmdaService
 
-__all__ = ["ProjectService", "SessionService", "SmdaService"]
+__all__ = ["ProjectService", "SmdaService"]
